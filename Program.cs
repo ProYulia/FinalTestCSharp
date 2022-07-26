596ecc24464eb06a51397948576889a348ad812a
Юлия <yu.s.p@yandex.ru>
2022-07-26 12:11:13 +0300

    code modifyed

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ string[] FindShortElements(string[] inputArray,int number)
     int CountShortElements(string[] inputArray)
     {
         int result = 0;
-        for(int i = 0; i < inputArray.Count(); i++)
+        for(int i = 0; i < inputArray.Length; i++)
         {
             if(inputArray[i].Length <= number) result++;
         }
